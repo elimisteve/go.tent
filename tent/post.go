74deeff1a9fd945177c5cf6d77992bcf3247a83b
Steve Phillips <elimisteve@gmail.com>
2012-10-02 09:03:06 -0700

    Shrank PostStatus() func dramatically thanks to 4 helper funcs

--- tent/post.go
+++ tent/post.go
@@ -19,39 +19,21 @@ const (
 )
 
 func PostStatus(currentAuthDetails, message string) ([]byte, error) {
-	mac := Mac{}
-	err := json.Unmarshal([]byte(currentAuthDetails), &mac)
-	fun.MaybeFatalAt("json.Unmarshal", err)
-
-	authStr := signRequest(&mac)
 	// TODO: JSON-escape `message`
 	body := fmt.Sprintf(STATUS_BODY, time.Now().Unix(), message)
-
-	// Server communication setup
-	client := &http.Client{}
-	// jsonStr, err := json.Marshal(body)
-	// fun.MaybeFatalAt("json.MaybeFatalAt", err)
-
-	reader := strings.NewReader(body)
-	req, err := http.NewRequest(ENV.Method, ENV.URL.String(), reader)
+	req, err := genRequest(body)
 	fun.MaybeFatalAt("http.NewRequest", err)
+	defer req.Body.Close()
 
-	// Add headers
-	for name, value := range STATUS_HEADERS {
-		req.Header.Add(name, value)
-	}
-	req.Header.Add("Authorization", authStr)
+	authStr, err := genAuthStr(currentAuthDetails)
+	fun.MaybeFatalAt("genAuthStr", err)
+	
+	addHeaders(req, authStr)
 
-	if DEBUG {
-		reqStr, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			fmt.Printf("Error reading from req.Body: %v\n", err)
-		}
-		fmt.Printf("Request body == '%s'\n\n", reqStr)
-		log.Fatalf("`client.Do` won't work anyway; stopping early\n")
-	}
+	if DEBUG { viewRequestBody(req) }
 
 	// Perform request
+	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("Error posting to %s: %v\n", ENV.URL.Host, err)
@@ -59,4 +41,36 @@ func PostStatus(currentAuthDetails, message string) ([]byte, error) {
 	jsonResp, err := ioutil.ReadAll(resp.Body)
 	fun.MaybeFatalAt("ioutil.ReadAll", err)
 	return jsonResp, nil
+}
+
+func addHeaders(req *http.Request, authStr string) {
+	for name, value := range STATUS_HEADERS {
+		req.Header.Add(name, value)
+	}
+	req.Header.Add("Authorization", authStr)
+}
+
+func viewRequestBody(req *http.Request) {
+	reqStr, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Printf("Error reading from req.Body: %v\n", err)
+	}
+	fmt.Printf("Request body == '%s'\n\n", reqStr)
+	log.Fatalf("`client.Do` won't work anyway; stopping early\n")
+}
+
+func genAuthStr(currentAuthDetails string) (string, error) {
+	mac := Mac{}
+	err := json.Unmarshal([]byte(currentAuthDetails), &mac)
+	if err != nil {
+		return "", err
+	}
+	authStr := signRequest(&mac)
+	return authStr, nil
+}
+
+func genRequest(body string) (*http.Request, error) {
+	reader := strings.NewReader(body)
+	req, err := http.NewRequest(ENV.Method, ENV.URL.String(), reader)
+	return req, err
 }
