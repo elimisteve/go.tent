97fb0c0e3ae82f83f3c965003a2d5e4d362a995f
Steve Phillips <elimisteve@gmail.com>
2012-10-04 13:26:32 -0700

    Removed evil global ENV var left over from version copying the Tent-authored, original Ruby client; uses tent.NewRequentInfo() instead.  Shows off convenient API and JSON unmarshaling.

--- sample-client.go
+++ sample-client.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/elimisteve/fun"
 	"github.com/elimisteve/go.tent/tent"
@@ -21,26 +22,41 @@ func main() {
 		log.Fatalf("Set USERNAME and CURRENT_AUTH_DETAILS in this file")
 	}
 
-	// TODO: Get rid of ENV. Don't copy the Ruby design; thread safety
-	// is good.
-	tent.ENV = tent.NewEnv(TENT_SERVER, "443", "/tent/posts", "post")
 	//
 	// Post new status update to TENT_SERVER
 	//
+	req := tent.NewRequestInfo(TENT_SERVER, "/tent/posts",
+		CURRENT_AUTH_DETAILS)
 	status := "This message posted with go.tent's sample client "
 	status += "https://github.com/elimisteve/go.tent"
-	responseBody, err := tent.PostStatus(CURRENT_AUTH_DETAILS, status)
+	responseBody, err := tent.PostStatus(req, status)
 	fun.MaybeFatalAt("tent.PostStatus", err)
-	fmt.Printf("Post from %s: \n%s\n", TENT_SERVER, responseBody)
+	fmt.Printf("Post from %s: \n%s\n\n", TENT_SERVER, responseBody)
 
+	// Unmarshal responseBody into a StatusPost
+	post := tent.StatusPost{}
+	err = json.Unmarshal(responseBody, &post)
+	fun.MaybeFatalAt("json.Unmarshal", err)
+	fmt.Printf("Unmarshal'd response: \n%+v\n", post)
 
-	tent.ENV = tent.NewEnv(TENT_SERVER, "443", "/tent/followings", "get")
 	//
-	// Get followers of TENT_SERVER
+	// Get users followed by TENT_SERVER
 	//
-	responseBody, err = tent.GetStatuses(CURRENT_AUTH_DETAILS)
+	req = tent.NewRequestInfo(TENT_SERVER, "/tent/followings",
+		CURRENT_AUTH_DETAILS)
+	responseBody, err = tent.Get(req)
 	fun.MaybeFatalAt("tent.GetStatuses", err)
-	fmt.Printf("\n\nWho %s is following: \n%s\n", TENT_SERVER, responseBody)
+	// fmt.Printf("%s\n\n\n", responseBody)
+
+	followings := []tent.Following{}
+	err = json.Unmarshal(responseBody, &followings)
+	fun.MaybeFatalAt("json.Unmarshal", err)
+
+	fmt.Printf("\n\n3 users %s is following:\n\n", TENT_SERVER)
+	// Loop over tent.Following vars
+	for _, f := range followings[:3] {
+		fmt.Printf("%#v\n\n", f)
+	}
 
 
 	//
