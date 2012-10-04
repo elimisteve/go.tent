e7c3e8ff398a94404127ed88d4bde66f079cb6f4
Steve Phillips <elimisteve@gmail.com>
2012-10-04 01:38:04 -0700

    Added tent/album.go

---
+++ tent/album.go
@@ -0,0 +1,11 @@
+// Steve Phillips / elimisteve
+// 2012.10.02
+
+package tent
+
+type Album struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Photos      []Id   `json:"photos"`      // Required
+	Cover       Id     `json:"cover"`
+}
