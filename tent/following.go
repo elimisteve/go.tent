d8acacaeeafc7d3a6c10e7ef9ca6873aed6a7d09
Steve Phillips <elimisteve@gmail.com>
2012-10-04 13:57:44 -0700

    Are you kidding me? 4th typo in tent/following.go; remoteid -> remote_id

--- tent/following.go
+++ tent/following.go
@@ -4,7 +4,7 @@
 package tent
 
 type Following struct {
-	RemoteId    string      `json:"remoteid,omitempty"`
+	RemoteId    string      `json:"remote_id,omitempty"`
 	Entity      Entity      `json:"entity,omitempty"`
 	Permissions Permissions `json:"permissions,omitempty"`
 	Id          Id          `json:"id,omitempty"`
