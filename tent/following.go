bf520b6d4bac90557b77cda8372539df506dad87
Steve Phillips <elimisteve@gmail.com>
2012-10-04 13:55:01 -0700

    Fixed another type in Following: {createdat,updatedat} -> {created_at,updated_at}

--- tent/following.go
+++ tent/following.go
@@ -8,8 +8,8 @@ type Following struct {
 	Entity      Entity      `json:"entity,omitempty"`
 	Permissions Permissions `json:"permissions,omitempty"`
 	Id          Id          `json:"id,omitempty"`
-	CreatedAt   int64       `json:"createdat,omitempty"`
-	UpdatedAt   int64       `json:"updatedat,omitempty"`
+	CreatedAt   int64       `json:"created_at,omitempty"`
+	UpdatedAt   int64       `json:"updated_at,omitempty"`
 	Groups      []Group     `json:"groups,omitempty"`
 	Profile     Profile     `json:"profile,omitempty"`
 	Licenses    []URL       `json:"licenses,omitempty"`
