c8a9a3444cde8c78355e41a5a0a49332d3928a08
Steve Phillips <elimisteve@gmail.com>
2012-10-04 01:38:05 -0700

    Added tent/content.go

---
+++ tent/content.go
@@ -0,0 +1,9 @@
+// Steve Phillips / elimisteve
+// 2012.10.02
+
+package tent
+
+type Content struct {
+	Types  []ProfileInfo `json:"types"`  // Required
+	Action string        `json:"action"` // Required
+}
