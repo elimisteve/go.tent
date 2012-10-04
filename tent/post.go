df042ce7ac97aec470b0ab9efc9c8016efe2c900
Steve Phillips <elimisteve@gmail.com>
2012-10-04 13:22:18 -0700

    Fixed type error, and improved names of vars, in Post func (in tent/post.go)

--- tent/post.go
+++ tent/post.go
@@ -17,12 +17,11 @@ func Post(info *RequestInfo, data string) ([]byte, error) {
 	defer req.Body.Close()
 
 	// Perform request
-	resp, err := genericRequest(req, info)
+	jsonResp, err := genericRequest(req, info)
 	if err != nil {
 		str := "Error calling genericRequest from Post: %v"
 		return nil, fmt.Errorf(str, err)
 	}
-	defer resp.Body.Close()
 
-	return resp, nil
+	return jsonResp, nil
 }
