4cae43f7fa091d45fbca73bc4fb342a8a3d1b12e
Steve Phillips <elimisteve@gmail.com>
2012-10-04 13:20:22 -0700

    Profile is now of type map[string]map[string]interface{}, which (Un)Marshals beautifully

--- tent/profile.go
+++ tent/profile.go
@@ -3,26 +3,62 @@
 
 package tent
 
-type Profile map[ProfileInfo]interface{}
+type Profile map[string]map[string]interface{}
+
+
+
+// // The idea here is to have BasicInfo and CoreInfo implement the
+// // `Marshal() ([]byte, error)` method, but it doesn't work:
+
+// type Profile map[ProfileInfo]json.Marshaler
+
+
+//
+// Unmarshal'd response
+//
+
+// map[string]interface {}{
+// 	"https://tent.io/types/info/basic/v0.1.0": map[string]interface {}{
+// 		"location":"Northeast Ohio",
+// 		"name":"Phil Jaenke",
+// 		"permissions":map[string]interface {}{"public":true},
+// 		"gender":"DB9",
+// 		"avatar_url":"",
+// 		"birthdate":"",
+// 		"bio":"AKA @rootwyrm. You know. Writer, storage, unix, backup, virtualization, PgSQL, performance monitoring. Personal account. (Resisting urge to make joke about pitching tent == personal. CRAP!)"
+// 	},
+// 	"https://tent.io/types/info/core/v0.1.0": map[string]interface {}{
+// 		"permissions": map[string]interface {}{"public":true},
+// 		"licenses":[]interface {}(nil),
+// 		"servers":[]interface {}{
+// 			"https://rootwyrm.tent.is/tent"},
+// 		"entity":"https://rootwyrm.tent.is"
+// 	}
+// }
+
+
+//
+// JSON version (actual response)
+//
 
 // "profile": {
 //         "https://tent.io/types/info/basic/v0.1.0": {
-//             "avatar_url": "https://si0.twimg.com/profile_images/2609036383/h3jgsk4sxozfp5ihop0d_reasonably_small.jpeg", 
-//             "bio": "Freelance web and mobile developer. Tent evangelist.", 
-//             "birthdate": "", 
-//             "gender": "", 
-//             "location": "The Netherlands", 
-//             "name": "Dave Clayton", 
+//             "avatar_url": "https://si0.twimg.com/profile_images/2609036383/h3jgsk4sxozfp5ihop0d_reasonably_small.jpeg",
+//             "bio": "Freelance web and mobile developer. Tent evangelist.",
+//             "birthdate": "",
+//             "gender": "",
+//             "location": "The Netherlands",
+//             "name": "Dave Clayton",
 //             "permissions": {
 //                 "public": true
 //             }
-//         }, 
+//         },
 //         "https://tent.io/types/info/core/v0.1.0": {
-//             "entity": "https://redskyforge.tent.is", 
-//             "licenses": [], 
+//             "entity": "https://redskyforge.tent.is",
+//             "licenses": [],
 //             "permissions": {
 //                 "public": true
-//             }, 
+//             },
 //             "servers": [
 //                 "https://redskyforge.tent.is/tent"
 //             ]
