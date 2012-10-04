050a715e62f40d29cb84a0ce1e11d934c049a7a8
Steve Phillips <elimisteve@gmail.com>
2012-10-04 12:55:12 -0700

    Content: Added several more fields, helpful comment, and omitempty

--- tent/content.go
+++ tent/content.go
@@ -3,7 +3,13 @@
 
 package tent
 
+// Content is a superset of Status, Repost, Profile, and Delete. Visit
+// http://tent.io/docs/post-types to see wtf I'm talking about.
 type Content struct {
-	Types  []ProfileInfo `json:"types"`  // Required
-	Action string        `json:"action"` // Required
+	Entity   Entity `json:"entity,omitempty"`
+	Id       Id     `json:"id,omitempty"`
+	Types    []URL  `json:"types,omitempty"`
+	Action   string `json:"action,omitempty"`
+	Text     string `json:"text,omitempty"`
+	Location Point  `json:"location,omitempty"`
 }
