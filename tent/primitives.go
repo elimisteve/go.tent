9449224a5664ce519fe2c84c23ac6ce7a54961b6
Steve Phillips <elimisteve@gmail.com>
2012-10-04 01:38:04 -0700

    Added tent/primitives.go

---
+++ tent/primitives.go
@@ -0,0 +1,19 @@
+// Steve Phillips / elimisteve
+// 2012.10.02
+
+package tent
+
+type Id string
+
+type Entity string
+
+// TODO: FIXME: Entities is WRONG; docs are ambiguous though
+type Entities struct{}
+
+// Latitude and Longitude
+type Point [2]float64
+
+type URL string
+
+// ???
+type Group string
