97386d56d6a45cf30ae7759146d70b547c6fe5c2
Steve Phillips <elimisteve@gmail.com>
2012-10-04 01:38:05 -0700

    Added tent/app.go

---
+++ tent/app.go
@@ -0,0 +1,13 @@
+// Steve Phillips / elimisteve
+// 2012.09.26
+
+package tent
+
+import (
+	// "net/http"
+)
+
+type App struct {
+	Name string `json:"name,omitempty"`
+	URL  string `json:"url,omitempty"`
+}
