261577a36453c46aae01515a7165e4cef2a652b4
Steve Phillips <elimisteve@gmail.com>
2013-01-13 14:00:58 -0800

    Fixed types of Entity and Id fields of Follower r struct

--- tent/follower.go
+++ tent/follower.go
@@ -4,7 +4,7 @@
 package tent
 
 type Follower struct {
-	Entity      string      `json:"entity,omitempty"`
-	Id          string      `json:"id,omitempty"`
+	Entity      Entity      `json:"entity,omitempty"`
+	Id          Id          `json:"id,omitempty"`
 	Permissions Permissions `json:"permissions,omitempty"`
 }
