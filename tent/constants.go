bfd7c0eb73ef1b596888333134964324613cd25d
Steve Phillips <elimisteve@gmail.com>
2013-01-13 13:54:37 -0800

    Removed ProfileInfo type and turned those variables into type URL; Made Post Type URLs versioned (v0.1.0 and v0.2.0); Added license more URLs

--- tent/constants.go
+++ tent/constants.go
@@ -3,25 +3,30 @@
 
 package tent
 
-// TODO: Probably rename `ProfileInfo` back to `TypeString`
-type ProfileInfo string
-
 // Info types
 const (
-	TYPE_CORE  ProfileInfo = "https://tent.io/types/info/core/v0.1.0"
-	TYPE_BASIC ProfileInfo = "https://tent.io/types/info/basic/v0.1.0"
+	TYPE_CORE  URL = "https://tent.io/types/info/core/v0.1.0"
+	TYPE_BASIC URL = "https://tent.io/types/info/basic/v0.1.0"
 )
 
 // Post types
 const (
-	TYPE_ALBUM    ProfileInfo = "https://tent.io/types/post/album/v0.1.0"
-	TYPE_DELETE   ProfileInfo = "https://tent.io/types/post/delete/v0.1.0"
-	TYPE_ESSAY    ProfileInfo = "https://tent.io/types/post/essay/v0.1.0"
-	TYPE_MENTIONS ProfileInfo = "https://tent.io/types/post/mentions/v0.1.0"
-	TYPE_PHOTO    ProfileInfo = "https://tent.io/types/post/photo/v0.1.0"
-	TYPE_PROFILE  ProfileInfo = "https://tent.io/types/post/profile/v0.1.0"
-	TYPE_REPOST   ProfileInfo = "https://tent.io/types/post/repost/v0.1.0"
-	TYPE_STATUS   ProfileInfo = "https://tent.io/types/post/status/v0.1.0"
+	TYPE_ALBUMv010    URL = "https://tent.io/types/post/album/v0.1.0"
+	TYPE_ALBUMv020    URL = "https://tent.io/types/post/album/v0.2.0"
+	TYPE_DELETEv010   URL = "https://tent.io/types/post/delete/v0.1.0"
+	TYPE_DELETEv020   URL = "https://tent.io/types/post/delete/v0.2.0"
+	TYPE_ESSAYv010    URL = "https://tent.io/types/post/essay/v0.1.0"
+	TYPE_ESSAYv020    URL = "https://tent.io/types/post/essay/v0.2.0"
+	TYPE_MENTIONSv010 URL = "https://tent.io/types/post/mentions/v0.1.0"
+	TYPE_MENTIONSv020 URL = "https://tent.io/types/post/mentions/v0.2.0"
+	TYPE_PHOTOv010    URL = "https://tent.io/types/post/photo/v0.1.0"
+	TYPE_PHOTOv020    URL = "https://tent.io/types/post/photo/v0.2.0"
+	TYPE_PROFILEv010  URL = "https://tent.io/types/post/profile/v0.1.0"
+	TYPE_PROFILEv020  URL = "https://tent.io/types/post/profile/v0.2.0"
+	TYPE_REPOSTv010   URL = "https://tent.io/types/post/repost/v0.1.0"
+	TYPE_REPOSTv020   URL = "https://tent.io/types/post/repost/v0.2.0"
+	TYPE_STATUSv010   URL = "https://tent.io/types/post/status/v0.1.0"
+	TYPE_STATUSv020   URL = "https://tent.io/types/post/status/v0.2.0"
 )
 
 // Licenses
@@ -32,13 +37,16 @@ const (
 	LICENSE_GPL_NEWEST       URL = "http://www.gnu.org/licenses/gpl.html"
 	LICENSE_AGPL_3           URL = "http://www.gnu.org/licenses/agpl-3.0.html"
 	LICENSE_AGPL_NEWEST      URL = "http://www.gnu.org/licenses/agpl.html"
+	LICENSE_PRIVATE          URL = "http://fakedomain.org/licenses/private.html"
+	LICENSE_LIMITED          URL = "http://fakedomain.org/intended-parties-only.html"
+	// TODO: Create private license
 )
 
 // Miscellaneous
 const (
-	MEDIA_TYPE  = "application/vnd.tent.v0+json"
-	PROFILE_REL = "https://tent.io/rels/profile"
-	VERSION     = "0.0.1"
+	MEDIA_TYPE      = "application/vnd.tent.v0+json"
+	PROFILE_REL URL = "https://tent.io/rels/profile"
+	VERSION         = "0.0.1"
 
 	NONCE_LENGTH = 6
 
